5d5721fdf2b5c5d7f7c5198568156d60627cb1a0
Risalah <121965268+2012ziadfawzan12@users.noreply.github.com>
2023-08-19 18:58:06 +0600

    Add files via upload

---
+++ blog/admin/class/function.php
@@ -0,0 +1,26 @@
+<?php
+
+ Class blogAdmin{
+
+           private $conn;
+
+        public function __construct()
+        {
+            #database host, database user, database pass, database name
+            $dbhost = 'localhost';
+            $dbuser = 'root';
+            $dbpass = "";
+            $dbname = 'blogproject';
+
+            $this->conn= mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+            
+            if(!$this->conn){
+                die("Database Connection Error!!");
+            }
+        }
+
+ }
+
+
+
+?>
